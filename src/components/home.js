75668c84012bf3185b384249d55c886cd21679c6
dino soifua <dinopearson801@gmail.com>
2018-08-02 10:58:35 -0600

    added exact route for home route and implemented withrouter HOC into the searchbar too navigate the user to resultsroute

--- src/components/home.js
+++ src/components/home.js
@@ -4,13 +4,15 @@ import Logo from './logo';
 import SearchBar from './searchBar';
 import RecentPosts from './recentPosts';
 
-export default class App extends Component {
+export default class Home extends Component {
   render() {
     return (
       <div>
-        <Logo/>
-        <SearchBar/>
-        <RecentPosts/>
+        <div>
+          <Logo/>
+          <SearchBar/>
+          <RecentPosts/>
+        </div>
       </div>
     );
   }
--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,15 +1,36 @@
+import React, { Component } from 'react';
+import { Field, reduxForm} from 'redux-form';
 
-import React, {Component} from 'react'
+import {withRouter} from 'react-router-dom';
 
-class SearchBar extends Component{
-    render(){
-        return(
-            <form>
-                <input placeholder="Seach DailySmarty"/>
+class SearchBar extends Component {
+
+    handleFormSubmit = function({query}) {
+        console.log('trying to handle submit for query', query);
+        //navigate to a new route.
+        this.props.history.push('./results');
+    }
+
+    renderInput(field) {
+        return <input type="text" placeholder="Search DailySmarty" {...field.input} />
+    }
+
+    render() {
+
+        const { handleSubmit } = this.props;
+
+        return (
+            <form className="search-bar" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+                <Field name="query" component={this.renderInput}/>
             </form>
-          
         )
     }
 }
+
+SearchBar = reduxForm({
+    form: 'searchBar'
+})(SearchBar);
+
+SearchBar = withRouter(SearchBar);
 
 export default SearchBar;
